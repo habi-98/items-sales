068b24bbbc93a3081ca4bf5857994123d00ffecb
Habi Sandurov <habi.alonso98@gmail.com>
2020-02-16 17:51:39 +0600

    connect mongoos

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const config = require('./config')
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+const port = 8000;
+
+
+mongoose.connect(config.dbuRL, config.mongoOptions).then(() => {
+    app.listen(port, () => {
+        console.log(`Server starded on ${port}`)
+    })
+})
+
+
+
